d1298c2704ad8517a8e68f30724d2f975b0e3504
wanglongkai <13699283312@163.com>
2023-07-27 10:10:14 +0800

    feat: 常用命令

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -29,8 +29,13 @@ export default defineConfig({
         text: 'dom & css',
         items: [
           { text: 'flex布局', link: '/domcss/flex.md' },
+          { text: '常用命令', link: '/domcss/常用命令.md' },
         ]
-      }
+      },
+      {
+        text: 'bug-fix',
+        link: '/bug/bug.md'
+      },
     ],
 
     socialLinks: [
---
+++ bug/bug.md
@@ -0,0 +1,8 @@
+# 居然不能以字符串形式出现`import.meta.env`
+
+[import.meta.env导致build失败](https://www.dianjilingqu.com/472031.html)
+
+vue组件中不能以字符串形式出现`import.meta.env.xxxx`。这会导致build失败。
+
+这真是一个奇葩的bug。
+
